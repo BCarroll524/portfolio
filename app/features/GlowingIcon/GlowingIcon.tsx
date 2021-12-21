6a59e4c8118f771e273bad298e21b28a8c2e0401
Blake Carroll <carroll.a.blake@gmail.com>
2021-12-20 23:42:29 -0800

    add hover effect on glowing icon

--- app/features/GlowingIcon/GlowingIcon.tsx
+++ app/features/GlowingIcon/GlowingIcon.tsx
@@ -9,10 +9,10 @@ type Props = {
 const GlowingIcon = ({ Icon }: Props) => {
   return (
     <div className="relative">
-      <div className="absolute top-0 scale-[130%] blur-md opacity-50">
+      <div className="absolute top-0 scale-105 blur-md opacity-50 hover:scale-125 transition-transform will-change-transform">
         {Icon}
       </div>
-      <div className="relative">{Icon}</div>
+      <div className="relative pointer-events-none">{Icon}</div>
     </div>
   );
 };
